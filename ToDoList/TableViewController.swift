028cb90e923b5f154d42f9ff7eae137c3a34c37a
Manohin <111537419+Manohin@users.noreply.github.com>
2023-07-10 16:46:45 +0300

    1

---
+++ ToDoList/TableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TableViewController.swift
+//  ToDoList
+//
+//  Created by Alexey Manokhin on 10.07.2023.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    
+    var tasks: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        tasks.count
+    }
+
+   
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = tasks[indexPath.row]
+
+        return cell
+    }
+  
+
+    @IBAction func createTaskButtonTapped(_ sender: UIBarButtonItem) {
+    }
+}
