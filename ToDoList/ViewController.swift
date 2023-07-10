9073c867c304322a7a5d70e8d2f7042a114a87a6
Manohin <111537419+Manohin@users.noreply.github.com>
2023-07-10 16:24:24 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Alexey Manokhin on 10.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
